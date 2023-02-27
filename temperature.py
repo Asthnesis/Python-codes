d8998276b125baea6b3476ec4c524a7a2c348ca4
Edward R <83113923+Asthnesis@users.noreply.github.com>
2023-02-27 19:02:41 +0300

    27th February 2023: Classes and Methods

---
+++ temperature.py
@@ -0,0 +1,16 @@
+""""Program with two methods, 1. convert_to_fahrenheit that coverts celcius to fahrenheit and
+2. convert_to_celcius that converts fahrenheit to celcius"""
+class Temperature:
+ def __init__(self,celcius,fahrenheit):
+    self.fahrenheit = fahrenheit
+    self.celcius = celcius
+
+ def convert_to_fahrenheit(self):
+   print((9/5*self.celcius)+32," F")
+
+ def convert_to_celcius(self):
+   print((self.fahrenheit-32)*5/9," C")
+
+T = Temperature(30,86)
+T.convert_to_celcius()
+T.convert_to_fahrenheit()
